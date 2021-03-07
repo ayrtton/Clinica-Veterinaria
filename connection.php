646e77dd17672c35ac649075b25152946087c054
ayrtt <ayrttoon@hotmail.com>
2021-03-07 11:23:26 -0300

    sql

--- connection.php
+++ connection.php
@@ -1,10 +1,15 @@
 <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "clinicaveterinaria";
 
-    $databaseHost = 'localhost';
-    $databaseName = 'clinicaveterinaria';
-    $databaseUsername = 'root';
-    $databasePassword = '';
-
-    $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-
+    try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+    }
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,28 @@
-<?php session_start(); ?>
+<?php
+    if(isset($errorMsg)) {
+        foreach($errorMsg as $error) {
+?>
+        <div class="alert alert-danger">
+            <strong><?php echo $error; ?></strong>
+        </div>
+<?php
+        }
+    }
+    if(isset($loginMsg)) {
+?>
+        <div class="alert alert-success">
+            <strong><?php echo $loginMsg; ?></strong>
+        </div>
+<?php
+    }
+?>
 
+
+
+<!doctype html>
 <html lang="en">
     <head>
-        <title>Clinica Veterinária</title>
+        <title>Login</title>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,18 +31,34 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        
-        <?php
-            if(isset($_SESSION['valid'])) {
-                include("connection.php");
-                $result = mysqli_query($mysqli, "SELECT * FROM funcionario");
-        ?> 
 
-        <?php
-            } else {
-                echo "Você precisa fazer Login para ver esta página.";
-            }
-        ?>
+        <form method="post" class="form-horizontal">
+            <div class="form-group">
+                <label class="col-sm-3 control-label">Username or Email</label>
+                <div class="col-sm-6">
+                    <input type="text" name="txt_username_email" class="form-control" placeholder="enter username or email">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-sm-3 control-label">Password</label>
+                <div class="col-sm-6">
+                    <input type="password" name="txt_password" class="form-control" placeholder="enter password">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-9 m-t-15">
+                    <input type="submit" name="btn_login" class="btn btn-success" value="Login">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-9 m-t-15">
+                    You don't have a account register here? <a href="register.php"><p class="text-info">Register Account</p></a>
+                </div>
+            </div>
+        </form>
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
